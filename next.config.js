9813bf29706e038485fc0dddf5fb8250a0721911
axelobscura <axosar@gmail.com>
2020-11-25 17:20:02 -0600

    Test db

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+      'MYSQL_HOST': '31.220.62.12',
+      'MYSQL_PORT': '3306',
+      'MYSQL_DATABASE': 'nextapp',
+      'MYSQL_USER': 'webart',
+      'MYSQL_PASSWORD': 'w2b1rt',
+    }
+  }
