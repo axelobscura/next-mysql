3c1993f0dbeb47f7941e5239dcaf1f90e6f30684
axelobscura <axosar@gmail.com>
2020-11-26 09:19:27 -0600

    Typeof

--- pages/api/get-entry.ts
+++ pages/api/get-entry.ts
@@ -7,6 +7,9 @@ const handler: NextApiHandler = async (req, res) => {
     if (!id) {
       return res.status(400).json({ message: '`id` required' })
     }
+    if (id !== 'number') {
+      return res.status(400).json({ message: '`id` must be a number' })
+    }
     const results = await query(
       `
       SELECT id, title, content
