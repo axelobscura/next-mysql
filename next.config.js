c84a1a1b9a83b5f12894c930a79fe953465c8b30
axelobscura <axosar@gmail.com>
2020-11-26 08:58:12 -0600

    conn

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     env: {
-      'MYSQL_HOST': '31.220.62.12',
+      'MYSQL_HOST': '185.28.21.241',
       'MYSQL_PORT': '3306',
-      'MYSQL_DATABASE': 'nextapp',
-      'MYSQL_USER': 'webart',
-      'MYSQL_PASSWORD': 'w2b1rt',
+      'MYSQL_DATABASE': 'u304720237_nextapp',
+      'MYSQL_USER': 'u304720237_nextapp',
+      'MYSQL_PASSWORD': 'N2xtt2st',
     }
   }
