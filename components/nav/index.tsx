ccbc3ff57339cbcd448f08878b82a9b5621e86ee
axelobscura <axosar@gmail.com>
2020-11-26 17:19:21 -0600

    Bootstrap

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -2,9 +2,21 @@ import Link from 'next/link'
 import Container from '@/components/container'
 import ButtonLink from '@/components/button-link'
 
+import Navbar from 'react-bootstrap/Navbar'
+
+
 export default function Nav({ title = 'Entries' }) {
   return (
-    <Container className="py-4">
+    <Container>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand><Link href="/">React-Bootstrap</Link></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Link href="/">
+            <a className="font-bold text-3xl">{title}</a>
+          </Link>
+        </Navbar.Collapse>
+      </Navbar>
       <nav>
         <div className="flex justify-between items-center">
           <Link href="/">
