3143331f3b1f31dbbac1f2636d95d2bfef849bc4
Axel <axosar@gmail.com>
2020-12-02 18:34:52 -0600

    Test

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ import PoweredByVercel from '../powered-by-vercel'
 function Footer() {
   return (
     <footer className="container mx-auto fixed bottom-0 right-0 left-0 flex justify-center items-center h-24">
-      APP DEVELOPMENT FROM AZUL.MX
+      APP DEVELOPMENT FROM AZUL.MX | 2020
     </footer>
   )
 }
