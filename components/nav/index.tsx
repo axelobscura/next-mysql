b6ee002431da1270c24ea05e193a19c438ccb795
Axel <axosar@gmail.com>
2020-12-07 17:38:12 -0600

    Header

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -9,11 +9,10 @@ export default function Nav({ title = 'Entries' }) {
   return (
     <Container>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand><Link href="/"><b>NEXT</b></Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Link href="/">
-            <a className="font-bold text-3xl">{title}</a>
+            <a className="font-light text-3xl">{title}</a>
           </Link>
         </Navbar.Collapse>
         <ButtonLink href="/new">New Entry</ButtonLink>
