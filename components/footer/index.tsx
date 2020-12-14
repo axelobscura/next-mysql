a640e6c186153160642c8f999f6755e43729b7ae
Axel <axosar@gmail.com>
2020-12-14 13:45:58 -0600

    Tester

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ import PoweredByVercel from '../powered-by-vercel'
 function Footer() {
   return (
     <footer className="container-fluid mx-auto fixed bottom-0 right-0 left-0 flex justify-center items-center h-20 elfooter">
-      <p>®2020 APP DEVELOPMENT FROM AZUL.MX - ALL RIGHTS RESERVED</p>
+      <p>®2020 APP DEVELOPMENT FROM AXEL LAURENT OBSCURA SARZOTTI - TODOS LOS DERECHOS RESERVADOS.</p>
     </footer>
   )
 }
