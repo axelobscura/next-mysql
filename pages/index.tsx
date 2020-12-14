4d7405be5664e6fdce6e25673395332c1f578242
Axel <axosar@gmail.com>
2020-12-14 10:32:44 -0600

    Entradas

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,28 +8,6 @@ import Layout from '@/components/layout'
 import { useEntries } from '@/lib/swr-hooks'
 
 export default function IndexPage() {
-  const { entries, isLoading } = useEntries()
-
-  if (isLoading) {
-    return (
-      <Layout home>
-        <div>
-          <Nav />
-          <Container>
-            <Skeleton width={180} height={24} />
-            <Skeleton height={48} />
-            <div className="my-4" />
-            <Skeleton width={180} height={24} />
-            <Skeleton height={48} />
-            <div className="my-4" />
-            <Skeleton width={180} height={24} />
-            <Skeleton height={48} />
-          </Container>
-        </div>
-      </Layout>
-    )
-  }
-
   return (
     <Layout home="home">
       <div>
