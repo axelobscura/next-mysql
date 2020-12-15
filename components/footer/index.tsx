3e2fa975902480b3fec09f8a2479dd0bf445032a
Axel <axosar@gmail.com>
2020-12-15 13:39:05 -0600

    Footer styles

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,7 +2,8 @@ import PoweredByVercel from '../powered-by-vercel'
 
 function Footer() {
   return (
-    <footer className="container-fluid mx-auto fixed bottom-0 right-0 left-0 flex justify-center items-center h-20 elfooter">
+    <footer className="container-fluid mx-auto fixed bottom-0 right-0 left-0 flex flex-column justify-center items-center h-20 elfooter">
+      <p>axosar@gmail.com</p>
       <p>®2020 APP DEVELOPMENT FROM AXEL LAURENT OBSCURA SARZOTTI - TODOS LOS DERECHOS RESERVADOS.</p>
     </footer>
   )
