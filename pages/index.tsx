2ff0334b26fb11cc250b864c673ed2dbf6d991f3
Axel <axosar@gmail.com>
2020-12-15 13:23:35 -0600

    Getting server data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import Nav from '@/components/nav'
 import Container from '@/components/container'
 import Jumbotronix from '@/components/jumbotron'
@@ -6,10 +7,10 @@ import Layout from '@/components/layout'
 import Link from 'next/link'
 import { useCategorias } from '@/lib/swr-hooks'
 
-function IndexPage() {
+function IndexPage({stars}) {
   const { categorias, isLoading } = useCategorias()
   console.log("Las categos: " + JSON.stringify(categorias))
-  
+  console.log("Las estrellas: " + JSON.stringify(stars))
   return (
     <Layout home="home">
       <div>
@@ -17,16 +18,23 @@ function IndexPage() {
         <Container className=" home">
           <Jumbotronix title="AXEL LAURENT OBSCURA SARZOTTI" />
           <div className="d-flex categorias">
-          {categorias ? categorias.map((e) => (
-            <div key={e.id} className="py-2">
-              <Link href={`/entry/${e.id}`}><p>{e.nombre}</p></Link>
-            </div>
-          )) : "LOADING DATA"}
+            {stars.map((e) => (
+              <div key={e.id} className="py-2">
+                <Link href={`/entry/${e.id}`}><p>{e.nombre}</p></Link>
+              </div>
+            ))
+            }
           </div>
         </Container>
       </div>
     </Layout>
   )
+}
+
+IndexPage.getInitialProps = async (ctx) => {
+  const res = await fetch('https://next-mysql-steel.vercel.app/api/get-categorias')
+  const json = await res.json()
+  return { stars: json }
 }
 
 export default IndexPage;
