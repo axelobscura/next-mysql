038150179bd5f9d2e2589e2edfc7ca3b390048a4
Axel <axosar@gmail.com>
2020-12-17 08:55:11 -0600

    Gris en hover

--- components/jumbotron/index.tsx
+++ components/jumbotron/index.tsx
@@ -7,7 +7,7 @@ export default function Jumbotronix({ title = 'Entries' }) {
       <Jumbotron fluid className="text-center eljumbo mb-0">
         <Container>
           <h1>{title}</h1>
-          <p>
+          <p className="intro">
             <b>SR. FULL STACK DEVELOPER</b>
           </p>
           <p className="pr-5 pl-5"><small>Senior Full Stack Developer with 17+ years of experience designing and developing backend architectures, decoupled and entier frontend web applications using HTML, CSS, JavaScript, JQuery, AJAX, JSON, XML, NodeJS, Express, PHP, MySQL, PosgressSQL, Angular, ReactJS, VueJS, NextJS, Ruby on Rails, Python, R, Mongo and much more...</small></p>
