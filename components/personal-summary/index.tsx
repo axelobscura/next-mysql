74a12b67e448d7b05557c43eb185564b49569dbe
Axel <axosar@gmail.com>
2020-12-17 18:54:25 -0600

    Fondo

--- components/personal-summary/index.tsx
+++ components/personal-summary/index.tsx
@@ -4,6 +4,7 @@ function PersonalSummary({datos}) {
   return (
     <Container className={` home ${datos.nombre}`}>
       <h1 style={{ color: "#fff"}}>{datos.nombre}</h1>
+      <p>Senior Full Stack Web Developer with 17+ years of experience designing and developing backend architectures, decoupled and n-tier frontend web applications using HTML, CSS, JavaScript, JQuery, AJAX, JSON, XML, NodeJS, Express, PHP, MySQL, PosgressSQL,  Angular, ReactJS, VueJS, NextJS… </p>
     </Container>
   )
 }
