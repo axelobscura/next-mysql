4442f1e93643408d320d27b05d19d42c76e9fa43
axelobscura <axosar@gmail.com>
2020-12-20 13:00:46 -0600

    pro summ

--- components/professional-summary/index.tsx
+++ components/professional-summary/index.tsx
@@ -6,7 +6,7 @@ import Col from 'react-bootstrap/Col'
 function ProfessionalSummary({datos}) {
   return (
     <Container className={` home ${datos.nombre}`}>
-      <h1 style={{ color: "#fff"}}><span className="lnr lnr-chevron-right"></span> {datos.nombre}</h1>
+      <h1><span className="lnr lnr-chevron-right"></span> {datos.nombre}</h1>
       <Devicons />
       <hr/>
       <Row>
