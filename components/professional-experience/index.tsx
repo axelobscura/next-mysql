fd1cb14d56542f8dc792ab1522ecd569a13e8eb3
axelobscura <axosar@gmail.com>
2021-01-02 16:05:56 -0600

    Layout

--- components/professional-experience/index.tsx
+++ components/professional-experience/index.tsx
@@ -8,6 +8,18 @@ function ProfessionalExperience({datos}) {
     <Container className={` home ${datos.nombre}`}>
       <h1><span className="lnr lnr-chevron-right"></span> {datos.nombre}</h1>
       <Devicons />
+      <ul>
+          <li>WEBINARS EN CONCRETO</li>
+          <li>ADIP - GOBIERNO DE LA CIUDAD DE MÉXICO</li>
+          <li>COLORCITI</li>
+          <li>CONGRESOS AMGG</li>
+          <li>PARALELO STUDIO</li>
+          <li>IMCYC</li>
+          <li>CONSTRUREMATES</li>
+          <li>DISTILLERY - THRIVE MARKET</li>
+          <li>TRYBÜ</li>
+          <li>AUTOGALLEGO</li>
+      </ul>
     </Container>
   )
 }
